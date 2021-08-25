dc1c9d3784c74224706618edde67b205de69893e
Ife <ifebrand6@gmail.com>
2021-08-25 01:11:07 +0100

    update picture  controller with rails 6

--- app/controllers/lines/admin/pictures_controller.rb
+++ app/controllers/lines/admin/pictures_controller.rb
@@ -6,15 +6,15 @@ module Lines
 
     class PicturesController < ApplicationController
       def create
-        @picture = Lines::Picture.create(picture_params)
+        @picture = Picture.create(picture_params)
       end
 
       # PUT /admin/pictures/1
       def update
-        @picture = Lines::Picture.find(params[:id])
+        @picture = Picture.find(params[:id])
 
         respond_to do |format|
-          if @picture.update_attributes(picture_params[:picture])
+          if @picture.update(picture_params[:picture])
             format.html { redirect_to @picture }
           else
             format.html { render action: "edit" }
@@ -24,7 +24,7 @@ module Lines
 
       # Deletes a picture. Only responds to JS requests.
       def destroy
-        @picture = Lines::Picture.find(params[:id])
+        @picture = Picture.find(params[:id])
         @picture.destroy
 
         respond_to do |format|
